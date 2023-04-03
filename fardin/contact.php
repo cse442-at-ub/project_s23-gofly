725d71e6b4df8286dd39cf73af49e497e9058823
Monty0252 <mamuin@buffalo.edu>
2023-04-03 01:17:42 -0400

    Task 46 Contact Us Page

--- fardin/contact.php
+++ fardin/contact.php
@@ -10,18 +10,17 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Plaster&family=Poppins:wght@200&display=swap" rel="stylesheet">
-    <title>Change Password</title>
+    <title>Login</title>
 </head>
 <body>
     <nav>
         <div class="logo">
-            <h4><a href="landing.html">Gofly</a></h4>
+            <h4><a href="landing2.html">Gofly</a></h4>
         </div>
         <ul class="nav-links">
             <li><a href="#">Reviews</a></li>
-            <li><a href="login.html">Login</a></li>
-            <li><a href="signup.html">Register</a></li>
             <li><a href="#">Contact Us</a></li>
+            <li><a href="signup.php">Register</a></li>
         </ul>
         <div class="burger">
             <div class="line1"></div>
@@ -31,19 +30,19 @@
     </nav>
 
     <div class="container">
-        <form action="signup.php" method="post" class="form">
-            <h2 style="font-size: 2rem;">Change Password</h2>
-            <input class="box" type="password" name="pwd" placeholder="Current Password" id = 'password'required>
-            <input class="box" type="password" name="pwd" placeholder="New Password" id = 'password'required>
-            <input class="box" type="password" name="pwd" placeholder="Confirm New Password" id = 'password'required>
+        <form action="https://formspree.io/f/mlekpwqw" method="post" class="form color-g">
+            <h2>Contact Us</h2>
+            
+            <input class="box" type="text" name="name" pattern="[a-zA-Z]+" title="Please enter only alphabetical letters." placeholder="Enter Your Name" required>
+            <input class="box" type="email" name="email" placeholder="Enter Your Email Address" required>
+            <textarea class="box" name="message" rows="4" cols="50" placeholder="Enter text here..."></textarea>
             <input type="submit" value="Submit" id="submit">
         </form>
         <div class="side">
             <img src="photos/bgpic1.png" alt="">
         </div>
     </div>
-
-    <script src="land.js"></script>
+</script>
     
 </body>
 </html>

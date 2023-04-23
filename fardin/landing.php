3dbf16452003d248dd68b61ed20d6e10f9553d24
fardinchowdhury <muh142001@gmail.com>
2023-04-22 21:29:05 -0400

    fixed bugs slideshow and navbar

--- fardin/landing.php
+++ fardin/landing.php
@@ -33,7 +33,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <title>Gofly</title>
 </head>
 <body>
-<?php 
+
+
+
+
+
+
+    <div id="slide-form2" class="crossfade">
+        <figure></figure>
+        <figure></figure>
+        <figure></figure>
+        <figure></figure>
+        <figure></figure>
+        <?php 
     // Check the session status
     $status = session_status();
 
@@ -49,7 +61,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         
     ?>
 
-    <div class="wel"> 
+        <div class="wel"> 
         <?php
 
         if(isset($_SESSION["username"])) {
@@ -59,123 +71,120 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         
         ?>
     </div>
+        <!-- .................Search Bar................................. -->
+        <div id="search-form">
+            <section>
+                <h2 class="header">Search Flights</h2>
+                <div class="flight" id="flightbox">
+                <?php if (!empty($errors)): ?>
+                <p class="error" style="color: red;"><?php echo $errors; ?></p>
+                <?php endif; ?>
 
+                    <form id="flight-form" method="post" action="">
+                        <!-- TRIP TYPE -->
+                        <div id="flight-type">
+                            <div class="info-box">
+                                <!-- <input
+                                    type="radio"
+                                    name="flight-type"
+                                    value="Return"
+                                    id="return"
+                                    checked="checked"/>
+                                <label for="return">
+                                    RETURN</label> -->
+                            </div>
+                            <div class="info-box">
+                                <input type="radio" name="flight-type" value="Single" id="one-way"/>
+                                <label for="one-way">ONE WAY</label>
+                            </div>
+                        </div>
 
+                        <!-- FROM/TO -->
+                        <div id="flight-depart">
+                            <div class="info-box">
+                                <label for="Origin">Origin</label>
+                                <select name="Origin" Placeholder="Select" >
+                                    <option value="JFK">JFK</option>
+                                    <option value="DAC">DAC</option>
+                                    <option value="SAF">SAF</option>
+                                    <option value="BOS">BOS</option>
+                                </select>
+                            </div>
+                            <div class="info-box">
+                                <label for="Destination">Destination</label>
+                                <select name="Destination" Placeholder="Select" >
+                                    <option value="JFK">JFK</option>
+                                    <option value="DAC">DAC</option>
+                                    <option value="SAF">SAF</option>
+                                    <option value="BOS">BOS</option>
+                                    <option value="BUF">BUF</option>
+                                </select>
+                            </div>
+                        </div>
 
+                        <!-- FROM/TO -->
+                        <div id="flight-dates">
+                            <div class="info-box">
+                                <label for="">Departure</label>
+                                <input
+                                    class="date-box"
+                                    type="date"
+                                    name="Departure"
+                                    class="form-control"
+                                    aria-describedby="return-date-label"/>
+                            </div>
 
-<!-- .................Search Bar................................. -->
-<div id="search-form">
-    <section>
-        <h2 class="header">Search Flights</h2>
-        <div class="flight" id="flightbox">
-        <?php if (!empty($errors)): ?>
-          <p class="error" style="color: red;"><?php echo $errors; ?></p>
-        <?php endif; ?>
+                            <!-- <div  class="info-box" id="return-box">
+                                <label for="">Arrival</label>
+                                <input
+                                    class="date-box"
+                                    type="date"
+                                    name="Arrival"
+                                    aria-describedby="return-date-label" required/>
+                            </div> -->
+                        </div>
 
-            <form id="flight-form" method="post" action="">
-                <!-- TRIP TYPE -->
-                <div id="flight-type">
-                    <div class="info-box">
-                        <!-- <input
-                            type="radio"
-                            name="flight-type"
-                            value="Return"
-                            id="return"
-                            checked="checked"/>
-                        <label for="return">
-                            RETURN</label> -->
-                    </div>
-                    <div class="info-box">
-                        <input type="radio" name="flight-type" value="Single" id="one-way"/>
-                        <label for="one-way">ONE WAY</label>
-                    </div>
+                        <!-- PASSENGER INFO -->
+                        <div id="flight-info">
+                            <div class="info-box">
+                                <label for="adults">Adults</label>
+                                <select name="adults">
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                </select>
+                            </div>
+                            <div class="info-box">
+                                <label for="class-type">Class</label>
+                                <select name="class-type">
+                                    <option value="Economy">Economy</option>
+                                    <option value="Business">Business</option>
+                                    <option value="First">First Class</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <!-- SEARCH BUTTON -->
+                        <div id="flight-search">
+                            <div class="info-box">
+                                <input type="submit" id="search-flight" name="search-flight" value="Search"/>
+                                <p class="failed"> 
+                                    <?php 
+                                        if(isset($_SESSION['status'])){ 
+                                            echo $_SESSION['status']; 
+                                            unset($_SESSION['status']); 
+                                        } 
+                                    ?> </p>
+                    
+                            </div>
+                        </div>
+                    </form>
+
                 </div>
-
-                <!-- FROM/TO -->
-                <div id="flight-depart">
-                    <div class="info-box">
-                        <label for="Origin">Origin</label>
-                        <select name="Origin" Placeholder="Select" >
-                            <option value="JFK">JFK</option>
-                            <option value="DAC">DAC</option>
-                            <option value="SAF">SAF</option>
-                            <option value="BOS">BOS</option>
-                        </select>
-                    </div>
-                    <div class="info-box">
-                        <label for="Destination">Destination</label>
-                        <select name="Destination" Placeholder="Select" >
-                            <option value="JFK">JFK</option>
-                            <option value="DAC">DAC</option>
-                            <option value="SAF">SAF</option>
-                            <option value="BOS">BOS</option>
-                            <option value="BUF">BUF</option>
-                        </select>
-                    </div>
-                </div>
-
-                <!-- FROM/TO -->
-                <div id="flight-dates">
-                    <div class="info-box">
-                        <label for="">Departure</label>
-                        <input
-                            class="date-box"
-                            type="date"
-                            name="Departure"
-                            class="form-control"
-                            aria-describedby="return-date-label"/>
-                    </div>
-
-                    <!-- <div  class="info-box" id="return-box">
-                        <label for="">Arrival</label>
-                        <input
-                            class="date-box"
-                            type="date"
-                            name="Arrival"
-                            aria-describedby="return-date-label" required/>
-                    </div> -->
-                </div>
-
-                <!-- PASSENGER INFO -->
-                <div id="flight-info">
-                    <div class="info-box">
-                        <label for="adults">Adults</label>
-                        <select name="adults">
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                        </select>
-                    </div>
-                    <div class="info-box">
-                        <label for="class-type">Class</label>
-                        <select name="class-type">
-                            <option value="Economy">Economy</option>
-                            <option value="Business">Business</option>
-                            <option value="First">First Class</option>
-                        </select>
-                    </div>
-                </div>
-
-                <!-- SEARCH BUTTON -->
-                <div id="flight-search">
-                    <div class="info-box">
-                        <input type="submit" id="search-flight" name="search-flight" value="Search"/>
-                        <p class="failed"> 
-                            <?php 
-                                if(isset($_SESSION['status'])){ 
-                                    echo $_SESSION['status']; 
-                                    unset($_SESSION['status']); 
-                                } 
-                            ?> </p>
-            
-                    </div>
-                </div>
-            </form>
-
+            </section>
         </div>
-    </section>
-</div>
+    </div>
 
 
 

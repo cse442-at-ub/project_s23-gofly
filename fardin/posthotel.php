a50f48456d8e5456ca1b991ec0a260eec494f8f0
Md Hyder <mdhyder@buffalo.edu>
2023-04-27 00:20:50 -0400

    Added type user to navbar #13

--- fardin/posthotel.php
+++ fardin/posthotel.php
@@ -72,7 +72,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="postHotel.css">
+    <link rel="stylesheet" href="post.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>

f91d57d871d3d38a191638fd898d6702ffe53bfc
Tahmid <rahmantahmid3@gmail.com>
2023-04-27 04:03:16 -0400

    api works; added css but more needed #67

--- fardin/weather.php
+++ fardin/weather.php
@@ -23,7 +23,8 @@ if (isset($_GET['id'])) {
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Plaster&family=Poppins:wght@200&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="display.css">
+    <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="landing.css">
     <link rel="stylesheet" href="weather.css">
     <title>Weather</title>
 </head>
@@ -32,44 +33,41 @@ if (isset($_GET['id'])) {
         include_once 'navbar.php';
     ?>
 
-    <h1>Weather Information</h1>
-
-    <div class="search-container">
-        <form action="" method="get">
+    <div class="container">
+        <form action="" method="get" class="form color-g">
             <input type="hidden" name="id" value="<?= $listing_id; ?>">
-            <input type="text" name="city" placeholder="Search city...">
-            <input type="submit" value="Search">
+            <h2 class="weather-title">Weather Information</h2>
+            <input class="weather-box" type="text" name="city" placeholder="Search city...">
+            <input type="submit" value="Search" id="weather-submit">
+
+            <script>
+                const apiKey = "5436f3bc399e7ad36a83b1bbb7f08afa";
+                const url = "https://api.openweathermap.org/data/2.5/weather";
+                const iconUrl = "https://openweathermap.org/img/wn";
+                const city = new URLSearchParams(window.location.search).get("city") || "New York";
+
+                fetch(`${url}?q=${city}&appid=${apiKey}&units=metric`)
+                    .then(response => response.json())
+                    .then(data => {
+                        const weatherContainer = document.getElementById("weather-container");
+                        const weatherIcon = data.weather[0].icon;
+
+                        weatherContainer.innerHTML = `
+                            <h2>${data.name}, ${data.sys.country}</h2>
+                            <img src="${iconUrl}/${weatherIcon}@2x.png" alt="${data.weather[0].description}" />
+                            <p>Temperature: ${data.main.temp}°C</p>
+                            <p>Feels like: ${data.main.feels_like}°C</p>
+                            <p>Weather: ${data.weather[0].description}</p>
+                        `;
+                    })
+                    .catch(error => {
+                        console.error("Error fetching weather data:", error);
+                    });
+            </script>
         </form>
+        <div class="side">
+            <img src="photos/bgpic1.png" alt="">
+        </div>
     </div>
-
-    <div id="weather-container">
-        <!-- Weather information will be displayed here -->
-        <p>Test</p>
-    </div>
-
-    <script>
-        const apiKey = "5436f3bc399e7ad36a83b1bbb7f08afa";
-        const url = "https://api.openweathermap.org/data/2.5/weather";
-        const iconUrl = "https://openweathermap.org/img/wn";
-        const city = new URLSearchParams(window.location.search).get("city") || "New York";
-
-        fetch(`${url}?q=${city}&appid=${apiKey}&units=metric`)
-            .then(response => response.json())
-            .then(data => {
-                const weatherContainer = document.getElementById("weather-container");
-                const weatherIcon = data.weather[0].icon;
-
-                weatherContainer.innerHTML = `
-                    <h2>${data.name}, ${data.sys.country}</h2>
-                    <img src="${iconUrl}/${weatherIcon}@2x.png" alt="${data.weather[0].description}" />
-                    <p>Temperature: ${data.main.temp}°C</p>
-                    <p>Feels like: ${data.main.feels_like}°C</p>
-                    <p>Weather: ${data.weather[0].description}</p>
-                `;
-            })
-            .catch(error => {
-                console.error("Error fetching weather data:", error);
-            });
-    </script>
 </body>
 </html>
